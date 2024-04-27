99c9f764ad2a6d5ee44ec691ee4a7f20bbb23e68
BenJ1337 <email@benjamin-hacker.de>
2024-04-27 13:30:08 +0200

    Only add modules, which aren't added already

---
+++ modules/3. Angebot/input.php
@@ -0,0 +1,51 @@
+<?php
+
+use redaxo_eingabekomponenten\{WYSIWYGEditor, Inputfield, Checkbox, Bildauswahl};
+use redaxo_bootstrap\{ModuleManager};
+
+$sliceId = -1;
+$output = '';
+
+$deckkraft = '95';
+$borderRadiusValue = '0';
+
+if (null !== $this->sliceSql && $this->mode === 'edit') {
+    $sliceId = $this->getCurrentSlice()->getId();
+    $rex_values_content = json_decode($this->getCurrentSlice()->getValue(2), true);
+    if (isset($rex_values_content["deckkraft"])) {
+        $deckkraft = $rex_values_content["deckkraft"];
+    }
+    if (isset($rex_values_content["borderRadius"])) {
+        $borderRadiusValue = $rex_values_content["borderRadius"];
+    }
+}
+
+$farbe = new Inputfield("Hintergrund",  ['hintergrund'],  $sliceId, 2);
+$farbe->setType('color');
+$output .= '<div class="form-group">' . $farbe->getHTML() . '</div>';
+
+$deck = new Inputfield("Deckkraft",  ['deckkraft'],  $sliceId, 2);
+$deck->setType('range');
+$deck->setSettings(array("min" => "0", "max" => "100", "value" => $deckkraft, "step" => "1"));
+$deck->addStyles(array('max-width' => '250px'));
+$output .= '<div class="form-group">' . $deck->getHTML() . '</div>';
+
+$borderRadius = new Inputfield("Abgerundete Ecken",  ['borderRadius'],  $sliceId, 2);
+$borderRadius->setType('range');
+$borderRadius->setSettings(array("min" => "0", "max" => "100", "value" => $borderRadiusValue, "step" => "0.1"));
+$borderRadius->addStyles(array('max-width' => '250px'));
+$output .= '<div class="form-group">' . $borderRadius->getHTML() . '</div>';
+
+$bildauswahl = new Bildauswahl("Bild",  ['bild'],  $sliceId, 2);
+$output .= '<div class="form-group">' . $bildauswahl->getHTML() . '</div>';
+
+$output .= '<div class="form-group">' . (new Checkbox(
+    "Inhalt zentrieren",
+    ["center"],
+    $sliceId,
+    1,
+    'center'
+))->getHTML() . '</div>';
+
+$output .= (new WYSIWYGEditor("Text",  ['text'],  $sliceId, 2))->getHTML();
+echo (new ModuleManager($sliceId))->getInput($output);
--- modules/3. Angebot/output.php
+++ modules/3. Angebot/output.php
@@ -1,1 +1,78 @@
-hello
+    <?php
+
+    use redaxo_bootstrap\{ModuleManager};
+
+    if (!function_exists('addText')) {
+        function addText($output, $slice)
+        {
+            $rex_values_content = json_decode($slice->getValue(2), true);
+            if (isset($rex_values_content["text"]) && $rex_values_content["text"] != '') {
+                $output .= '<div class="col-md-6">';
+                $output .= $rex_values_content["text"];
+                $output .= '</div>';
+            }
+            return $output;
+        }
+    }
+
+    if (!function_exists('addImage')) {
+        function addImage($output, $slice)
+        {
+            $rex_values_content = json_decode($slice->getValue(2), true);
+            if (isset($rex_values_content["bild"]) && $rex_values_content["bild"] != '') {
+                $alt = $rex_values_content["bild"];
+                $medium = rex_media::get($rex_values_content["bild"]);
+                if ($medium != null) {
+                    $alt = $medium->getTitle() != "" ? trim($medium->getTitle()) : $medium->getFileName();
+                }
+                $output .= '<div class="col-md-6">';
+                $root = rex::getServer();
+                if (str_ends_with($root, '/')) {
+                    $root = substr($root, 0, strlen($root) - 1);
+                }
+                $output .= '<img style="width: 100%" class="img-fluid" src="'
+                    . $root . '/media/' . $rex_values_content["bild"] . '" alt="' . $alt . '">';
+                $output .= '</div>';
+            }
+            return $output;
+        }
+    }
+
+    if (!function_exists('addContent')) {
+        function addContent($slice)
+        {
+            $output = '';
+            $rex_values_content = json_decode($slice->getValue(2), true);
+            if (isset($rex_values_content["text"]) && $rex_values_content["text"] != '') {
+                $styles = '';
+                if (!rex::isBackend() && isset($rex_values_content["borderRadius"]) && isset($rex_values_content["borderRadius"])) {
+                    $styles .=  'border-radius: ' . $rex_values_content["borderRadius"] . 'px; padding: 20px 10px; margin: 0 0 10px; ';
+                }
+                if (!rex::isBackend() && isset($rex_values_content["hintergrund"]) && isset($rex_values_content["deckkraft"])) {
+                    list($r, $g, $b) = sscanf($rex_values_content["hintergrund"], "#%02x%02x%02x");
+                    $styles .= 'background: rgba(' . $r . ',' . $g . ',' . $b . ',' . $rex_values_content["deckkraft"] / 100 . ');';
+                    $output .= '<div class="row">';
+                }
+
+                $output .= '<div class="col">';
+                $output .= '<div class="row" style="' . $styles . '">';
+                $output = addImage($output, $slice);
+                $output = addText($output, $slice);
+                $output .= '</div></div>';
+
+                if (!rex::isBackend() && isset($rex_values_content["hintergrund"]) && isset($rex_values_content["deckkraft"])) {
+                    $output .= '</div>';
+                }
+            }
+            return (new ModuleManager($slice->getId()))->getOutput($output);
+        }
+    }
+
+    //rex_article_content:: getContentAsQuery(true) -> SQL Queries erlauben
+    //rex_article_content::
+    $this->getContentAsQuery(true);
+    $slice = rex_article_slice::getArticleSliceById($this->getCurrentSlice()->getId());
+    if (null !== $slice) {
+        echo addContent($slice);
+    }
+    ?>

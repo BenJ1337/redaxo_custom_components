078e184e1dc000c6e764e0e4e86a431e52b67869
BenJ1337 <email@benjamin-hacker.de>
2024-04-27 15:08:06 +0200

    refactored process of setting default value

--- modules/1. Text (Jodit)/input.php
+++ modules/1. Text (Jodit)/input.php
@@ -6,33 +6,26 @@ use redaxo_bootstrap\{ModuleManager};
 $sliceId = -1;
 $output = '';
 
-$deckkraft = '95';
-$borderRadiusValue = '0';
-
 if (null !== $this->sliceSql && $this->mode === 'edit') {
     $sliceId = $this->getCurrentSlice()->getId();
-    $rex_values_content = json_decode($this->getCurrentSlice()->getValue(2), true);
-    if (isset($rex_values_content["deckkraft"])) {
-        $deckkraft = $rex_values_content["deckkraft"];
-    }
-    if (isset($rex_values_content["borderRadius"])) {
-        $borderRadiusValue = $rex_values_content["borderRadius"];
-    }
 }
 
 $farbe = new Inputfield("Hintergrund",  ['hintergrund'],  $sliceId, 2);
-$farbe->setType('color');
+$farbe->setType(Inputfield::TYPE_COLOR);
+$farbe->setDefaultValue('#FFFFFF');
 $output .= $farbe->getHTML();
 
 $deck = new Inputfield("Deckkraft",  ['deckkraft'],  $sliceId, 2);
-$deck->setType('range');
-$deck->setSettings(array("min" => "0", "max" => "100", "value" => $deckkraft, "step" => "1"));
-$output .=   $deck->getHTML();
+$deck->setType(Inputfield::TYPE_RANGE);
+$deck->setSettings(array("min" => "0", "max" => "100", "step" => "1"));
+$deck->setDefaultValue(95);
+$output .= $deck->getHTML();
 
 $borderRadius = new Inputfield("Abgerundete Ecken",  ['borderRadius'],  $sliceId, 2);
-$borderRadius->setType('range');
-$borderRadius->setSettings(array("min" => "0", "max" => "100", "value" => $borderRadiusValue, "step" => "0.1"));
-$output .=   $borderRadius->getHTML();
+$borderRadius->setType(Inputfield::TYPE_RANGE);
+$borderRadius->setDefaultValue(0);
+$borderRadius->setSettings(array("min" => "0", "max" => "100", "step" => "0.1"));
+$output .= $borderRadius->getHTML();
 
 $output .= '<div class="form-group">' . (new Checkbox(
     "Inhalt zentrieren",
